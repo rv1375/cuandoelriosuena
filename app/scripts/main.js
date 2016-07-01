1cf1623e86dd763a50f52260706427319db7e7ac
Marlon <marlon.alvarado@hotmail.com>
2016-07-01 14:46:24 -0500

    Menú

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,39 +1,14 @@
-$(document).ready(function () {
-    $(document).on("scroll", onScroll);
+// on click event on all anchors with a class of scrollTo
+$('a.scrollTo').on('click', function(){
+  
+  // data-scrollTo = section scrolling to name
+  var scrollTo = $(this).attr('data-scrollTo');
+  
+  // animate and scroll to the sectin 
+  $('body, html').animate({
     
-    //smoothscroll
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-        
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-      
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
-});
-
-function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('#menu a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#menu ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-    });
-}
+    // the magic - scroll to section
+    "scrollTop": $('#'+scrollTo).offset().top
+  }, 1000 );
+  return false;
+})
